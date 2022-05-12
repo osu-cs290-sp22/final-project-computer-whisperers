e827671fdb3fced0790416680e4104b06d063ece
StanleyHale <82476210+StanleyCHale@users.noreply.github.com>
2022-05-11 21:46:55 -0700

    So i did some super basic text scrolling implimentation from a package I found here: https://github.com/tameemsafi/typewriterjs/blob/master/readme.md
    I hope adding all this shit works for others on github lol...

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // JS Stuff Here
+//Variables
+var textChat = document.getElementById('text-Chat');
+var textBoxContainer = document.getElementById('text-Box');
+
+var temporaryText = document.getElementById('text-Chat').textContent;
+
+//Functions
+
+//Code
+var typewriter = new Typewriter(textChat, {
+  loop: false,
+  delay: 75,
+});
+
+textBoxContainer.addEventListener('click', ()=> {
+  typewriter
+  .typeString(temporaryText)
+  .start();
+})
