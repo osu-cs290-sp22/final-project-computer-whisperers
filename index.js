ec775b559767b8fe181d73de27548c741baf5efb
StanleyHale <82476210+StanleyCHale@users.noreply.github.com>
2022-05-12 17:41:10 -0700

    Added a little intro piece

--- index.js
+++ index.js
@@ -7,30 +7,52 @@ var textBoxContainer = document.getElementById('text-Box');
 
 var temporaryText = document.getElementById('text-Chat').textContent;
 
+var introCounter = -1;
+var introText = [
+  "Once again... Awoken in your bed on your birthday. All alone in your room.",
+  "Will there ever be a time where this scene will have changed?",
+  "No, I won't let this be my fate. I am determined to find some friends to call family!",
+  "I must set out on a quest to make some friends...",
+  "There have been reaccuring rumors about a door between realities that leads to a land of all the friends you could ever imagine!",
+  "Maybe I should head out to the village nearby to procure more information of this 'door'."
+];
+
 //Functions
+  function introSequence() {
+    if(introCounter <= 6) {
+      //Set Intro to 0 to start text if it isnt already.
+      if(introCounter == -1) {
+        introCounter = 0;
+      }
+      if (textBoxClicked == 0) {
+        typewriter
+        .changeDelay(25)
+        .typeString(introText[introCounter])
+        .start();
+        textBoxClicked = 1;
+        introCounter++;
+      } else {
+        //textChat.textContent='';
+        //
+        typewriter
+        .deleteAll(1)
+        .start();
+        //
+        textBoxClicked = 0;
+
+      }
+    }
+  }
 
 //Code
 var typewriter = new Typewriter(textChat, {
   loop: false,
+  deleteSpeed: 1,
   delay: 75,
+
 });
 
 textBoxContainer.addEventListener('click', ()=> {
-  console.log(textBoxClicked);
-  if (textBoxClicked == 0) {
-    typewriter
-    .changeDelay(25)
-    .typeString(temporaryText)
-    .start();
-    textBoxClicked = 1;
-  } else {
-    //textChat.textContent='';
-    //
-    typewriter
-    .changeDeleteSpeed(25) //Doesnt actually change delete speed for some reason?
-    .deleteAll()
-    .start();
-    //
-    textBoxClicked = 0;
-  }
+//Intro handling
+  introSequence();
 })
