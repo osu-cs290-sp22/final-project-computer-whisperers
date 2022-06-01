e1a4ab7b1e4ecf5003b0868f11010785152cef22
StanleyHale <82476210+StanleyCHale@users.noreply.github.com>
2022-06-01 14:26:18 -0700

    Started messing around with server stuff... Need to reorganize some files into a public/ directory.

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
 //JS Stuff Here
+
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var reviewsData = require('./reviewsData.json');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs.engine({ defaultLayout: null}) );
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+//Code:
+
+app.get('/', function (req, res, next) {
+  //console.log('shiz1');
+  //res.status(200).sendfile(path.join(__dirname,'public', 'index.html'));
+  //console.log(twitDataTemp);
+  res.status(200).render('gamePage');
+
+});
+
+app.get('*', function (req, res) {
+  res.status(404).render('./404.html')
+});
+
+
+
+app.listen(port, function () {
+  console.log("== Server is listening on port", port);
+});
