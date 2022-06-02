5a6aa8d9180d9e6d4d44ab307851a88dbfb29089
StanleyHale <82476210+StanleyCHale@users.noreply.github.com>
2022-06-01 18:40:27 -0700

    Finished servee side implimentation from a server side perspective for creatring reviews!

--- public/reviews.js
+++ public/reviews.js
@@ -4,7 +4,9 @@
 function createReview(body, author) {
   let today = new Date();
   let date = (today.getMonth()+1) + '/' + today.getDate() + '/' + today.getFullYear();
-  let time = today.getHours() + ":" + today.getMinutes();
+  let hours = today.getHours();
+  hours = (hours % 12) || 12;
+  let time = hours + ":" + today.getMinutes();
   if( (today.getHours() >= 0) && (today.getHours() < 12) ) {
     time = time+" AM";
   }
@@ -13,19 +15,39 @@ function createReview(body, author) {
   }
   console.log("Time of review creation: ", date, time);
 
-  var newReview = Handlebars.templates.review( {
-    date: date,
-    time: time,
-    body: body,
-    author: author
-  });
+  fetch('/Friendventure/reviews/addReview', {
+      method: 'POST',
+      body: JSON.stringify({
+        "date": date,
+        "time": time,
+        "body": body,
+        "author": author
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+  }).then(function (res) {
+    if(res.status === 200) {
+      var newReview = Handlebars.templates.review( {
+        date: date,
+        time: time,
+        body: body,
+        author: author
+      });
 
-  //console.log('--New review: ', newReview);
+      //console.log('--New review: ', newReview);
 
-  var reviewContainer = document.getElementById('review-container');
-  //console.log('--Review Container: ', reviewContainer);
-  reviewContainer.insertAdjacentHTML('beforeend', newReview);
-}
+      var reviewContainer = document.getElementById('review-container');
+      //console.log('--Review Container: ', reviewContainer);
+      reviewContainer.insertAdjacentHTML('beforeend', newReview);
+    }
+    else {
+      alert("ERROR || Problem with response from server from adding a Rewview!!!");
+    }
+  }).catch(function (err) {
+    alert("ERROR || Cannot communicate with server!");
+  })
+};
 
 
 
